e54343348b9228cdb28af21133c680d3d8432cd1
geeeger <lph1988520@sina.com>
2020-11-04 11:02:04 +0800

    feat(iso-3166-1): move to here

--- packages/get-gmt8-local-date/__tests__/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import getGMT8LocalDate from '../src';
-
-describe('@geeeger/get-gmt8-local-date module', () => {
-  it('should be ', () => {
-    expect(getGMT8LocalDate(Math.ceil(Date.now() / 1000))).toBeInstanceOf(Date);
-  });
-});
---
+++ packages/iso-3166-1/__tests__/index.spec.ts
@@ -0,0 +1,7 @@
+import target from '../src';
+
+describe('@geeeger/iso-3166-1 module', () => {
+  it('should exist', () => {
+    expect(target).toBeDefined()
+  });
+});
