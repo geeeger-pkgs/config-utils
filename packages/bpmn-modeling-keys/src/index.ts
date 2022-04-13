19a02b981d0e58428bb98721c2f04bcc1e6dd415
geeeger <lph1988520@sina.com>
2022-04-13 14:01:06 +0800

    feat:add bpmn-modeling-keys

---
+++ packages/bpmn-modeling-keys/src/index.ts
@@ -0,0 +1,48 @@
+export default {
+    shape: {
+        append: 'shape.append',
+        create: 'shape.create',
+        delete: 'shape.delete',
+        move: 'shape.move',
+        resize: 'shape.resize',
+        replace: 'shape.replace',
+        toggleCollapse: 'shape.toggleCollapse',
+    },
+    spaceTool: 'spaceTool',
+    label: {
+        create: 'label.create',
+    },
+    connection: {
+        create: 'connection.create',
+        delete: 'connection.delete',
+        move: 'connection.move',
+        layout: 'connection.layout',
+        updateWaypoints: 'connection.updateWaypoints',
+    },
+    elements: {
+        create: 'elements.create',
+        move: 'elements.move',
+        delete: 'elements.delete',
+        distribute: 'elements.distribute',
+        align: 'elements.align',
+    },
+    element: {
+        updateAttachment: 'elements.updateAttachment',
+        updateModdleProperties: 'element.updateModdleProperties',
+        updateProperties: 'element.updateProperties',
+        setColor: 'element.setColor',
+        updateLabel: 'element.updateLabel',
+    },
+    canvas: {
+        updateRoot: 'canvas.updateRoot',
+    },
+    lane: {
+        add: 'lane.add',
+        resize: 'lane.resize',
+        split: 'lane.split',
+        updateRefs: 'lane.updateRefs',
+    },
+    id: {
+        updateClaim: 'id.updateClaim',
+    },
+}
